250320786e84605f4296323a9c64b2c0df783a2e
Slawomir Jaranowski <slawomir.jaranowski@payu.com>
2023-03-04 10:15:33 +0100

    GitHub repository - publish

--- set_git_user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-git config --local user.name  "`git log -1 --format="%an"`"
-git config --local user.email "`git log -1 --format="%ae"`"
-
-echo "Git user settings"
-git config --local --get-regexp user.*
